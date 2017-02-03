4fcd23ce300b5061858219f86c86cb949ec78d7c
murej <jure.martinec@gmail.com>
2017-02-03 17:22:27 +0100

    Update style

--- src/GiftedChat.js
+++ src/GiftedChat.js
@@ -33,7 +33,7 @@ const MIN_COMPOSER_HEIGHT = Platform.select({
   android: 41,
 });
 const MAX_COMPOSER_HEIGHT = 100;
-const MIN_INPUT_TOOLBAR_HEIGHT = 41 + 5;
+const MIN_INPUT_TOOLBAR_HEIGHT = 40 + 4;
 
 class GiftedChat extends React.Component {
   constructor(props) {
